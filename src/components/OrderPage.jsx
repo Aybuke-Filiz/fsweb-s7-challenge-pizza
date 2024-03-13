78311c1a92f5058b06d6d85f7884c69e8a65246c
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-03-13 23:02:03 +0300

    testler öncesi

--- src/components/OrderPage.jsx
+++ src/components/OrderPage.jsx
@@ -18,6 +18,12 @@ function OrderPage() {
   const [quantity, setQuantity] = useState(1);
   const [totalPrice, setTotalPrice] = useState(0);
   const basePrice = 85.50;
+  const [errors, setErrors] = useState({
+    name: '',
+    toppings: '',
+    size: '',
+    thickness: ''
+  });
 
   const handleQuantityChange = (event) => {
     const newQuantity = parseInt(event.target.value);
@@ -38,177 +44,210 @@ function OrderPage() {
     const totalPrice = (basePrice + sizePrice + toppingsPrice) * quantity;
     setTotalPrice(totalPrice);
   };
-const history=useHistory();
+  
+  const history=useHistory();
+
   const handleSubmit = () => {
-    if (!name || toppings.length < 4 || !size) {
-      console.error('Lütfen tüm gerekli alanları doldurun.');
-      return;
+    let valid = true;
+
+    
+    if (name.length < 3) {
+      setErrors(prevErrors => ({
+        ...prevErrors,
+        name: "Adınızı en az 3 karakter giriniz."
+      }));
+      valid = false;
+    } else {
+      setErrors(prevErrors => ({
+        ...prevErrors,
+        name: ""
+      }));
     }
 
-    const orderData = {
-      name: name,
-      toppings: toppings,
-      thickness:thickness,
-      size: size,
-      notes: notes,
-      quantity: quantity
-    };
+   
+    if (toppings.length < 4 || toppings.length > 10) {
+      setErrors(prevErrors => ({
+        ...prevErrors,
+        toppings: "En az 4 en fazla 10 adet seçim yapabilirsiniz."
+      }));
+      valid = false;
+    } else {
+      setErrors(prevErrors => ({
+        ...prevErrors,
+        toppings: ""
+      }));
+    }
 
-    axios.post('https://reqres.in/api/pizza', orderData)
-      .then(response => {
-        console.log('Sipariş bilgileri başarıyla gönderildi.');
-        console.log('Sunucu yanıtı:', response.data);
-        console.log('Sipariş Özeti:');
-        console.log('İsim:', response.data.name);
-        console.log('Ek Malzemeler:', response.data.toppings.join(', '));
-        console.log('Boyut Seç:', response.data.size);
-        console.log('Hamur Seç:', response.data.thickness);
-        console.log('Notlar:', response.data.notes);
-        console.log('Sipariş Tarihi:', response.data.createdAt);
-        console.log('Adet:', response.data.quantity);
-        console.log('Toplam Fiyat:', response.data.totalPrice);
-        history.push("/SuccesPage");
-      })
-      .catch(error => {
-        console.error('Sipariş gönderilirken bir hata oluştu:', error);
-      });
+    
+    if (!size) {
+      setErrors(prevErrors => ({
+        ...prevErrors,
+        size: "Lütfen bir boyut seçiniz."
+      }));
+      valid = false;
+    } else {
+      setErrors(prevErrors => ({
+        ...prevErrors,
+        size: ""
+      }));
+    }
+
+    
+    if (!thickness) {
+      setErrors(prevErrors => ({
+        ...prevErrors,
+        thickness: "Lütfen bir hamur kalınlığı seçiniz."
+      }));
+      valid = false;
+    } else {
+      setErrors(prevErrors => ({
+        ...prevErrors,
+        thickness: ""
+      }));
+    }
+
+    if (valid) {
+      const orderData = {
+        name: name,
+        toppings: toppings,
+        thickness:thickness,
+        size: size,
+        notes: notes,
+        quantity: quantity
+      };
+  
+      axios.post('https://reqres.in/api/pizza', orderData)
+        .then(response => {
+          console.log('Sunucu yanıtı:', response.data);
+          console.log('İsim:', response.data.name);
+          console.log('Ek Malzemeler:', response.data.toppings.join(', '));
+          console.log('Boyut Seç:', response.data.size);
+          console.log('Hamur Seç:', response.data.thickness);
+          console.log('Notlar:', response.data.notes);
+          console.log('Sipariş Tarihi:', response.data.createdAt);
+          console.log('Adet:', response.data.quantity);
+          console.log('Toplam Fiyat:', response.data.totalPrice);
+          history.push("/SuccesPage");
+        })
+        .catch(error => {
+          console.error('Sipariş gönderilirken bir hata oluştu:', error);
+        });
+    }
   };
 
   return (
-    
-    <div class="card-container">
+    <div className="card-container">
       <header>
-      <h1>Teknolojik Yemekler</h1>
+        <h1>Teknolojik Yemekler</h1>
       </header>
       <p>Anasayfa-Sipariş oluştur</p>
-      <div class="metin-container">
-      <h2>Position Absolute Acı Pizza</h2>
-      <p class="tutar"> {basePrice} TL</p>
-      <p class="paragraf">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, quis blanditiis vero dolores quibusdam perspiciatis quisquam amet. Numquam hic ab ducimus quam laudantium dolores perspiciatis quis asperiores eaque modi fugit ad vero quo, quasi placeat, aliquid quidem nulla! Maiores, quae nobis? Vero possimus consequuntur nihil natus voluptatem quod. Eos laudantium at ipsa consequuntur amet blanditiis, vel ea debitis dolorem iste! At quasi aperiam nesciunt iusto eius explicabo molestias cupiditate, animi nemo debitis molestiae eligendi dolore hic mollitia accusamus. Accusantium, tempora ut iste illum consectetur culpa dolores cum officia magnam at. Deleniti, excepturi voluptates? Eaque aliquid adipisci mollitia, quam cupiditate cum?</p>
+      <div className="metin-container">
+        <h2>Position Absolute Acı Pizza</h2>
+        <p className="tutar"> {basePrice} TL</p>
+        <p className="paragraf">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, quis blanditiis vero dolores quibusdam perspiciatis quisquam amet. Numquam hic ab ducimus quam laudantium dolores perspiciatis quis asperiores eaque modi fugit ad vero quo, quasi placeat, aliquid quidem nulla! Maiores, quae nobis? Vero possimus consequuntur nihil natus voluptatem quod. Eos laudantium at ipsa consequuntur amet blanditiis, vel ea debitis dolorem iste! At quasi aperiam nesciunt iusto eius explicabo molestias cupiditate, animi nemo debitis molestiae eligendi dolore hic mollitia accusamus. Accusantium, tempora ut iste illum consectetur culpa dolores cum officia magnam at. Deleniti, excepturi voluptates? Eaque aliquid adipisci mollitia, quam cupiditate cum?</p>
       
-      <form>
-      <FormGroup tag="fieldset">
-    <legend>
-      Boyut Seç
-    </legend>
-      <FormGroup check>
-      <Input
-        name="size"
-        type="radio"
-        value="small"
-        checked={size === 'small'}
-        onChange={() => setSize('small')} 
-      />
-      {' '}
-      <Label check>
-        Küçük
-      </Label>
-    </FormGroup>
-    <FormGroup check>
-      <Input
-        name="size"
-        type="radio"
-        value="medium"
-        checked={size === 'medium'}
-        onChange={() => setSize('medium')} 
-      />
-      {' '}
-      <Label check>
-        Orta
-      </Label>
-</FormGroup>
-<FormGroup check>
-      <Input
-        name="size"
-        type="radio"
-        value="large"
-        checked={size === 'large'}
-        onChange={() => setSize('Büyük')} 
-      />
-      {' '}
-      <Label check>
-        Büyük
-      </Label>
-</FormGroup>
-    </FormGroup>
-        <FormGroup>
-    <Label for="exampleSelect">
-      Hamur Seç
-    </Label>
-    <Input
-      id="exampleSelect"
-      name="select"
-      type="select"
-      onChange={e => setThickness(e.target.value)} 
-    >
-      <option>
-        İnce
-      </option>
-      <option>
-        Orta
-      </option>
-      <option>
-        Kalın
-      </option>
-    </Input>
-  </FormGroup>
+        <form>
+          <FormGroup tag="fieldset">
+            <legend>Boyut Seç</legend>
+            <FormGroup check>
+              <Input
+                name="size"
+                type="radio"
+                value="small"
+                checked={size === 'small'}
+                onChange={() => setSize('small')} 
+              />
+              <Label check>Küçük</Label>
+            </FormGroup>
+            <FormGroup check>
+              <Input
+                name="size"
+                type="radio"
+                value="medium"
+                checked={size === 'medium'}
+                onChange={() => setSize('medium')} 
+              />
+              <Label check>Orta</Label>
+            </FormGroup>
+            <FormGroup check>
+              <Input
+                name="size"
+                type="radio"
+                value="large"
+                checked={size === 'large'}
+                onChange={() => setSize('large')} 
+              />
+              <Label check>Büyük</Label>
+            </FormGroup>
+            {errors.size && <span className="error">{errors.size}</span>}
+          </FormGroup>
 
-        <label>Ek Malzemeler:</label><br></br>
-        <input type="checkbox" id="pepperoni" value="pepperoni" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Pepperoni
-        <input type="checkbox" id="Sosis" value="Sosis" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Sosis
-        <input type="checkbox" id="Kanada Jambonu" value="Kanada Jambonu" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Kanada Jambonu
-        <input type="checkbox" id="Tavuk Izgara" value="Tavuk Izgara" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Tavuk Izgara<br></br>
-        <input type="checkbox" id="Soğan" value="Soğan" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Soğan
-        <input type="checkbox" id="Domates" value="Domates" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Domates
-        <input type="checkbox" id="Mısır" value="Mısır" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Mısır
-        <input type="checkbox" id="Sucuk" value="Sucuk" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Sucuk
-        <input type="checkbox" id="Jalepeno" value="Jalepeno" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Jalepeno
-        <input type="checkbox" id="Sarımsak" value="Sarımsak" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Sarımsak<br></br>
-        <input type="checkbox" id="Biber" value="Biber" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Biber
-        <input type="checkbox" id="Sucuk" value="Sucuk" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Sucuk
-        <input type="checkbox" id="Köz Biber" value="Köz Biber" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Köz Biber
-        <input type="checkbox" id="Turşu" value="Turşu" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Turşu<br></br>
-        <FormGroup>
-    <Label for="Adınız">
-      Adınız
-    </Label>
-    <Input
-      id="name"
-      name="ad"
-      placeholder="Adınızı giriniz"
-      type="text"
-      minLength={3}
-      value={name}
-      onChange={e => setName(e.target.value)}
-    />
-     </FormGroup>
-     <FormGroup>
-    <Label for="notes">
-      Sipariş Notu
-    </Label>
-    <Input
-      id="notes"
-      name="notes"
-      placeholder="Siparişine eklemek istediğin bir not var mı?"
-      type="textarea"
-      value={notes}
-      onChange={e => setNotes(e.target.value)} 
-      
-    />
-     </FormGroup>
+          <FormGroup>
+            <Label for="exampleSelect">Hamur Seç</Label>
+            <Input
+              id="exampleSelect"
+              name="select"
+              type="select"
+              onChange={e => setThickness(e.target.value)} 
+            >
+              <option>İnce</option>
+              <option>Orta</option>
+              <option>Kalın</option>
+            </Input>
+            {errors.thickness && <span className="error">{errors.thickness}</span>}
+          </FormGroup>
 
-       
-        <label htmlFor="quantity">Adet:</label>
-        <input type="number" id="quantity" value={quantity} onChange={handleQuantityChange} min="1" />
+          <label>Ek Malzemeler:</label><br />
+          <input type="checkbox" id="pepperoni" value="pepperoni" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Pepperoni
+          <input type="checkbox" id="Sosis" value="Sosis" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Sosis
+          <input type="checkbox" id="Kanada Jambonu" value="Kanada Jambonu" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Kanada Jambonu
+          <input type="checkbox" id="Tavuk Izgara" value="Tavuk Izgara" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Tavuk Izgara<br />
+          <input type="checkbox" id="Soğan" value="Soğan" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Soğan
+          <input type="checkbox" id="Domates" value="Domates" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Domates
+          <input type="checkbox" id="Mısır" value="Mısır" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Mısır
+          <input type="checkbox" id="Sucuk" value="Sucuk" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Sucuk
+          <input type="checkbox" id="Jalepeno" value="Jalepeno" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Jalepeno
+          <input type="checkbox" id="Sarımsak" value="Sarımsak" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Sarımsak<br />
+          <input type="checkbox" id="Biber" value="Biber" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Biber
+          <input type="checkbox" id="Köz Biber" value="Köz Biber" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Köz Biber
+          <input type="checkbox" id="Turşu" value="Turşu" onChange={e => setToppings(prevToppings => e.target.checked ? [...prevToppings, e.target.value] : prevToppings)} /> Turşu<br />
 
-        <label>Toplam Fiyat: {totalPrice} TL</label>
+          <FormGroup>
+            <Label for="Adınız">Adınız</Label>
+            <Input
+              id="name"
+              name="ad"
+              placeholder="Adınızı giriniz"
+              type="text"
+              minLength={3}
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+            {errors.name && <span className="error">{errors.name}</span>}
+          </FormGroup>
 
-        <button type="button" class="siparis"onClick={handleSubmit}>Sipariş Ver</button>
-      </form>
-    </div>
+          <FormGroup>
+            <Label for="notes">Sipariş Notu</Label>
+            <Input
+              id="notes"
+              name="notes"
+              placeholder="Siparişine eklemek istediğin bir not var mı?"
+              type="textarea"
+              value={notes}
+              onChange={e => setNotes(e.target.value)} 
+            />
+          </FormGroup>
+
+          <label htmlFor="quantity">Adet:</label>
+          <input type="number" id="quantity" value={quantity} onChange={handleQuantityChange} min="1" />
+
+          <label>Toplam Fiyat: {totalPrice} TL</label>
+
+          <button type="button" className="siparis" onClick={handleSubmit}>Sipariş Ver</button>
+        </form>
+      </div>
     </div>
   );
 }
 
-
-export default OrderPage
+export default OrderPage;
